646d4c688a2ca21be1175bac22cf5be54e28a5cc
Jesus F. Trujillo <jesusft@yahoo-inc.com>
2015-05-20 23:06:32 -0700

    separate bundles for background and ui

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp         = require('gulp');
+var clean        = require('gulp-clean');
 var gutil        = require('gulp-util');
 var sass         = require('gulp-sass');
 var source       = require('vinyl-source-stream');
@@ -11,22 +12,25 @@ var browserify   = require('browserify');
 var browserSync  = require('browser-sync');
 
 // Input file.
-var bundler     = watchify(browserify('./app/js/app.jsx', watchify.args));
+var uiBundler = watchify(browserify('./app/js/app.jsx', watchify.args));
+var bgBundler = watchify(browserify('./app/js/background.js', watchify.args));
 
 // React JSX transform
-bundler.transform(reactify);
+uiBundler.transform(reactify);
 
 // Babel, 6to5ify transform
-bundler.transform(sixtofiveify);
+uiBundler.transform(sixtofiveify);
+bgBundler.transform(sixtofiveify);
 
 // On updates recompile
-bundler.on('update', bundleJS);
+uiBundler.on('update', bundleUIJS);
+bgBundler.on('update', bundleBgJS);
 
-function bundleJS() {
+function bundleUIJS() {
 
-    gutil.log('Compiling JS...');
+    gutil.log('Compiling UI JS...');
 
-    return bundler.bundle()
+    return uiBundler.bundle()
         .on('error', function (err) {
             gutil.log(err.message);
             browserSync.notify("Browserify Error!");
@@ -40,23 +44,42 @@ function bundleJS() {
         .pipe(browserSync.reload({stream: true, once: true}));
 }
 
+function bundleBgJS() {
+
+    gutil.log('Compiling background JS...');
+
+    return bgBundler.bundle()
+        .on('error', function (err) {
+            gutil.log(err.message);
+            browserSync.notify("Browserify Error!");
+            this.emit("end");
+        })
+        .pipe(source('background.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+        .pipe(sourcemaps.write('./')) // writes .map file
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.reload({stream: true, once: true}));
+}
+
 /**
  * Gulp task alias
  */
 gulp.task('bundle', function () {
-    return bundleJS();
+    bundleBgJS();
+    return bundleUIJS();
 });
 
 gulp.task('sass', function () {
-  gulp.src('./app/styles/**/*.scss')
-    .pipe(sourcemaps.init())
-        .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/css'));
+    gulp.src('./app/styles/**/*.scss')
+        .pipe(sourcemaps.init())
+            .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./app/styles/**/*.scss', ['sass']);
+    gulp.watch('./app/styles/**/*.scss', ['sass']);
 });
 
 gulp.task('copy', function() {
@@ -64,12 +87,17 @@ gulp.task('copy', function() {
         .pipe(gulp.dest('build/fonts'));
     gulp.src('app/icons/**')
         .pipe(gulp.dest('build/icons'));
-    gulp.src('app/_locales/**')
-        .pipe(gulp.dest('build/_locales'));
+    //gulp.src('app/_locales/**')
+    //   .pipe(gulp.dest('build/_locales'));
     gulp.src('app/index.html')
         .pipe(gulp.dest('build'));
     return gulp.src('app/manifest.json')
         .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('build')
+        .pipe(clean());
 });
 
 /**
